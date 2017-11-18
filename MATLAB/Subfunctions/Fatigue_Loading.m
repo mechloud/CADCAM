c0085d68ebb2e83fcadef3cf75a9c3c79ec1fec1
liztheloon <eprim068@uottawa.ca>
2017-11-18 14:32:33 -0500

    Fatigue Loading Code
    Just for the stress loads to check for infinite life

---
+++ MATLAB/Subfunctions/Fatigue_Loading.m
@@ -0,0 +1,15 @@
+function Fatigue_Loading(sigma_max, sigma_min, S_e, S_ut, S_y)
+
+%%Calculate alternating and midrange stresses
+sigma_a = (sigma_max - sigma_min)/2;
+sigma_m = (sigma_max + sigma_min)/2;
+
+%%Find SF for modified goodman equation
+
+SF_mod = 1/(sigma_a/S_e + sigma_m/S_ut)
+
+%%Find SF for Langer Yield
+
+SF_lan = S_y/(sigma_a + sigma_m)
+
+End
