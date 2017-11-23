f6c77f4521d71abab98e3e3a943d9d7021f0ffee
Justin McLeod <jmcle078@uottawa.ca>
2017-11-23 16:45:38 -0500

    changes to bolt tool

--- MATLAB/+tools/BoltTool.m
+++ MATLAB/+tools/BoltTool.m
@@ -1,23 +1,29 @@
-%% BoltTool
+﻿%% BoltTool
 % BOLTTOOL Takes a struct input and calculates applicable stresses for
 % semi-permanent fasteners
-function BoltTool(b,n)
+function BoltTool()
 
 %%
 % If the number of input arguments is less than 2, declare defaults. This
 % is used for debugging and testing.
 if nargin < 2
-   b = struct('bdia',12.7,... % Bolt Diameter in mm
-              'F',1300,...    % Shearing Force in N
-              't',0.125,...    % Thickness of member
-              'mxA',200,...   % Cross sectional area of weakeast connected member
-              'SyM',600);     % Yield Strength of weakest member
-   n = 2.0;
+   b = struct('F',1340,...    % Shearing Force in N
+              't',6.08,...    % Thickness of member
+              'mxA',240,...   % Cross sectional area of weakeast connected member
+              'SyM',250);     % Yield Strength of weakest member
+   n = 3.0;
 end
 
+bdia = 5.9; %mm initial bolt size
+nbB = 0;
+ntau = 0;
+
+while (nbB < n) || (ntau < n)
+    
+bdia = bdia+.1;
 %% Pure Shear Failure Mode
 % Find cross sectional area of bolt
-xA = (pi/4)*b.bdia^4;
+xA = (pi/4)*bdia^4;
 
 %%
 % Determine shear stress
@@ -30,11 +36,11 @@ sigmaM = b.F/b.mxA;
 
 %% Crushing (Bearing Failure) of Bolt or Member
 % Determine bearing stress in bolt
-bearing_sigmaB = -b.F/b.bdia;
+bearing_sigmaB = -b.F/bdia;
 
 %%
 % Determine bearing stress in member
-bearing_sigmaM = -b.F/(b.bdia*b.t);
+bearing_sigmaM = -b.F/(bdia*b.t);
 
 %% Safety Factors
 % Calculate safety factor for tensile failure of member
@@ -44,24 +50,17 @@ nMemTensile = b.SyM/sigmaM;
 % Calculate safety factor for bearing load in member
 nMemBearing = b.SyM/bearing_sigmaM;
 
-%%
-% Ensure these safety factors are higher than input safety factor
 assert(abs(nMemTensile) > n,'Possible Tensile Failure of Member');
 assert(abs(nMemBearing) > n,'Possible Bearing Failure of Member');
-
 %%
 % Declare Proof Loads for Grade 4.8 Bolts
 Sp = 310;
 nbB = Sp/abs(bearing_sigmaB);
 ntau = Sp/tau;
 
-if (ntau > n) && (nbB > n)
-    disp('Bolts okay!');
-else
-    disp('Bolts not okay...');
-    fprintf('Safety Factor for shear %d\n',ntau);
-    fprintf('Safety Factor for bearing %d\n',nbB);
 end
+
+fprintf('Bolt Diameter is %d mm.\n',bdia);
 
 end % End function
 
