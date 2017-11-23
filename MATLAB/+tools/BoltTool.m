40f70aa7bd6c79d7e827bd1028ad882f78128725
Justin McLeod <jmcle078@uottawa.ca>
2017-11-23 17:26:59 -0500

    added database search of bolt size

--- MATLAB/+tools/BoltTool.m
+++ MATLAB/+tools/BoltTool.m
@@ -1,4 +1,4 @@
-﻿%% BoltTool
+%% BoltTool
 % BOLTTOOL Takes a struct input and calculates applicable stresses for
 % semi-permanent fasteners
 function BoltTool()
@@ -7,20 +7,22 @@ function BoltTool()
 % If the number of input arguments is less than 2, declare defaults. This
 % is used for debugging and testing.
 if nargin < 2
-   b = struct('F',1340,...    % Shearing Force in N
+   b = struct('F',700,...    % Shearing Force in N
               't',6.08,...    % Thickness of member
               'mxA',240,...   % Cross sectional area of weakeast connected member
               'SyM',250);     % Yield Strength of weakest member
-   n = 3.0;
+   n = 4.0;
 end
 
-bdia = 5.9; %mm initial bolt size
+sizes = load('Bolt_Sizes.mat');
+size = sizes.Bolt_Sizes(:,1);
+i = 1; %counter
+
 nbB = 0;
 ntau = 0;
 
 while (nbB < n) || (ntau < n)
-    
-bdia = bdia+.1;
+bdia = size(i);    
 %% Pure Shear Failure Mode
 % Find cross sectional area of bolt
 xA = (pi/4)*bdia^4;
@@ -58,9 +60,10 @@ Sp = 310;
 nbB = Sp/abs(bearing_sigmaB);
 ntau = Sp/tau;
 
+i = i + 1;
 end
 
-fprintf('Bolt Diameter is %d mm.\n',bdia);
+fprintf('Bolt Diameter is %d mm.\n',size(i-1));
 
 end % End function
 
