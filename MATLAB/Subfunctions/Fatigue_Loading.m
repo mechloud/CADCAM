03ca125ee25381f531631c355e1f93e44b0ba492
johncharbo95 <jonathan.charbo@hotmail.com>
2017-12-05 08:38:34 -0500

    Formatting

--- MATLAB/Subfunctions/Fatigue_Loading.m
+++ MATLAB/Subfunctions/Fatigue_Loading.m
@@ -1,15 +1,16 @@
 function [SF_mod, SF_lan] = Fatigue_Loading(sigma_max, sigma_min, S_e, S_ut, S_y)
 
-%%Calculate alternating and midrange stresses from Equations (\ref{eq:sigma_a}) and (\ref{eq:sigma_m})
+%%
+% Calculate alternating and midrange stresses from Equations (\ref{eq:sigma_a}) and (\ref{eq:sigma_m})
 sigma_a = (sigma_max - sigma_min)/2;
 sigma_m = (sigma_max + sigma_min)/2;
 
-%%Find SF for modified goodman equation from Equation (\ref{eq:mod_good})
-
+%%
+% Find SF for modified goodman equation from Equation (\ref{eq:mod_good})
 SF_mod = 1/(sigma_a/S_e + sigma_m/S_ut);
 
-%%Find SF for Langer Yield from Equation (\ref{eq:langer})
-
+%%
+% Find SF for Langer Yield from Equation (\ref{eq:langer})
 SF_lan = S_y/(sigma_a + sigma_m);
 
 end
