4c83b8e807db532a1b609ddef48d7d0eae2883da
Justin McLeod <jmcle078@uottawa.ca>
2017-12-05 20:24:28 -0500

    added parantheses

--- MATLAB/+tools/BoltTool.m
+++ MATLAB/+tools/BoltTool.m
@@ -31,50 +31,50 @@ xA = (pi/4)*bdia^2;
 
 %%
 % Determine shear stress
-% From Equation \ref{eq:pureshear}
+% From Equation (\ref{eq:pureshear})
 tau = b.F/xA;
 
 %% Tensile Failure of Member
 % Determine tensile stress of member using cross-sectional area of attached
 % member/plate with area of bolt substracted.
-% From Equation \ref{eq:tension}
+% From Equation (\ref{eq:tension})
 sigmaM = b.F/b.mxA;
 
 %% Crushing (Bearing Failure) of Bolt or Member
 % Determine bearing stress in bolt
-% From Equation \ref{eq:bearing}
+% From Equation (\ref{eq:bearing})
 bearing_sigmaB = -b.F/(bdia*b.t);
 
 %%
 % Determine bearing stress in member
-% From Equation \ref{eq:bearing}
+% From Equation (\ref{eq:bearing})
 bearing_sigmaM = -b.F/(bdia*b.t);
 
 %% Safety Factors
 % Calculate safety factor for tensile failure of member
-% From Equation \ref{eq:ntension}
+% From Equation (\ref{eq:ntension})
 nMemTensile = b.SyM/sigmaM;
 
 %%
 % Calculate safety factor for bearing load in member
-% From Equation \ref{eq:nbearing}
+% From Equation (\ref{eq:nbearing})
 nMemBearing = b.SyM/bearing_sigmaM;
 
 %%
 % Declare Proof Loads for Grade 4.8 Bolts
 Sp = 310;
-% From Equation \ref{eq:nbearing}
+% From Equation (\ref{eq:nbearing})
 nbB = b.SyM/abs(bearing_sigmaB);
-% From Equation \ref{eq:nshear}
+% From Equation (\ref{eq:nshear})
 ntau = Sp/tau;
 
 %%
 % Bending Stress
-% From Equation \ref{eq:moment}
+% From Equation (\ref{eq:moment})
 Mmax=(blength*b.F)/4;
-% From Equation \ref{eq:bend}
+% From Equation (\ref{eq:bend})
 sigmaB=(Mmax*(bdia/2))/((pi*bdia^4)/64);
-% From Equation \ref{eq:nbend}
+% From Equation (\ref{eq:nbend})
 nbend = Sp/sigmaB;
 
 %%
