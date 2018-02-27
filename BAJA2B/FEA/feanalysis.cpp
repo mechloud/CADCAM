322910b8ae41c2624e92d5ff35251600aa292553
Jonathan Charbonneau <jonathan.charbo@hotmail.com>
2018-02-26 19:02:47 -0500

    Added solver class and post-processing, but massive bug somewhere...

--- BAJA2B/FEA/feanalysis.cpp
+++ BAJA2B/FEA/feanalysis.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<Eigen/Core>
+#include<Eigen/Dense>
 #include<math.h> // needed for M_PI in operations class
 #include<string>
 #include<sstream>
@@ -12,7 +13,6 @@ struct Node{
   int id;
   double x;
   double y;
-  double z;
 };
 
 struct Element{
@@ -52,7 +52,7 @@ auto read_csv(const std::string& nodal, const std::string& elemental){
 
     Node n;
     
-    ss >> n.id >> n.x >> n.y >> n.z;
+    ss >> n.id >> n.x >> n.y;
     
     // if (fabs(n.y) > 1.0e-12){
     //   throw std::runtime_error("Error, node has y component. Code is not ready for 3D analysis yet.");
@@ -99,7 +99,8 @@ class TwoDBeam {
 public:
 
   using matrix_type = Eigen::MatrixXd;
-  static constexpr int number_of_unknowns = 6;
+  using vector_type = Eigen::VectorXd;
+  static constexpr int number_of_unknowns = 3;
 
   /////////////////////////////////////////////////
   // Build Rotation Matrix
@@ -107,7 +108,7 @@ public:
   template<typename T>
   static matrix_type rotation(const T& l, const T& m){
     matrix_type R;
-    R.resize(number_of_unknowns,number_of_unknowns);
+    R.resize(2*number_of_unknowns,2*number_of_unknowns);
     R.fill(0.0);
 
     R(0,0) = l;
@@ -125,7 +126,7 @@ public:
     R(4,4) = l;
     
     R(5,5) = 1;
-
+    
     return R;
   }
 
@@ -136,7 +137,7 @@ public:
   static matrix_type stiffness(const T& A, const T& E, const T& L, const T& I){
     
     matrix_type Ke;
-    Ke.resize(number_of_unknowns,number_of_unknowns);
+    Ke.resize(2*number_of_unknowns,2*number_of_unknowns);
     Ke.fill(0.0);
 
     Ke(0,0) = A*E/L;
@@ -145,7 +146,7 @@ public:
     Ke(1,1) = 12.0*E*I/(L*L*L);
     Ke(1,2) = 6.0*E*I/(L*L);
     Ke(1,4) = -Ke(1,1);
-    Ke(1,5) = -Ke(1,2);
+    Ke(1,5) = Ke(1,2);
 
     Ke(2,1) = Ke(1,2);
     Ke(2,2) = 4.0*E*I/L;
@@ -153,7 +154,7 @@ public:
     Ke(2,5) = 0.5*Ke(2,2);
 
     Ke(3,0) = -Ke(0,0);
-    Ke(3,3) = -Ke(0,3);
+    Ke(3,3) = Ke(0,0);
     
     Ke(4,1) = -Ke(1,1);
     Ke(4,2) = -Ke(1,2);
@@ -167,6 +168,19 @@ public:
 
     return Ke;
   }
+
+  static vector_type force_vector(const double& m, const int& n){
+    vector_type F(n*number_of_unknowns);   
+    F.fill(0.0);
+    // Might need to double check this, should be 5,8,12 and 13 according to MATLAB
+    F(4*number_of_unknowns) = 20818.0;
+    F(11*number_of_unknowns) = 20818.0;
+    F(12*number_of_unknowns + 1) = -m * 9.81;
+    F(7*number_of_unknowns + 1) = -26.0 * 9.81;
+    
+    return F;
+  }
+  
 
 }; // End TwoDBeam class
 
@@ -199,6 +213,8 @@ public:
     TT0 dy = y2-y1;
     TT0 L = sqrt(dx*dx + dy*dy);
 
+    //std::cout << "x1 " << x1 << "\ty1 " << y1 << "\t\tx2 " << x2 << "\ty2 " << y2 << '\n'; 
+
     if(L <= 0){
       throw std::runtime_error("Zero-length or negative length element");
     } else {
@@ -220,7 +236,7 @@ public:
   
 private:
   T POD = 25.4;
-  T PID = 19.304;
+  T PID = 19;
   T SOD = 25.4;
   T SID = 20.638;
   T md  = 110;
@@ -244,17 +260,28 @@ public:
     md(md_in)
   {};
 
+  auto SolveSystem (const std::vector<Node>& nodes, const std::vector<Element>& elements){
+    auto Ka = preprocessing(nodes,elements);
+    std::tie(U,F) = solve(Ka,nodes.size());
+    auto SF = postprocessing(U,elements);
+
+    return 69;
+  }
+
   
-  void preprocessing(const std::vector<Node>& nodes,const std::vector<Element>& elements){
+  auto preprocessing(const std::vector<Node>& nodes,const std::vector<Element>& elements){
 
     operations<double> op;
 
     typename Equations::matrix_type rotated;
     typename Equations::matrix_type assembled;
 
-    double nelements = elements.size();
-    R.resize(nelements);
-    Ke.resize(nelements);
+    int nnodes = nodes.size();
+    int nelements = elements.size();
+    static constexpr int block_size = Equations::number_of_unknowns;
+    int ka_size = nnodes * block_size;
+    Ka.resize(ka_size,ka_size);
+    Ka.fill(0.0);
 
     for (int i = 0; i < nelements; ++ i){
 
@@ -270,20 +297,78 @@ public:
 
       // Get length and find direction cosines
       L.push_back(op.get_length(xi,yi,xj,yj));
+      std::cout << "L[" << i << "] = " << L[i] << '\n';
       l.push_back((xj-xi)/L[i]);
       m.push_back((yj-yi)/L[i]);
 
       // Get rotation matrices
-      R[i] = Equations::rotation(l[i],m[i]);
+      R.push_back(Equations::rotation(l[i],m[i]));
 
-      // Get local stiffness matrix;
+      // Get local stiffness matrix
       Eigen::MatrixXd Ke_prime = Equations::stiffness(op.area(POD,PID),E,L[i],op.inertia(POD,PID));
 
-      Ke[i] = R[i].transpose() * Ke_prime * R[i];
+      // Compute global stiffness matrix
+      Ke.push_back(R[i].transpose() * Ke_prime * R[i]);
 
-      std::cout << Ke[i] << "\n\n";
+      // For indexing assemblage matrix
+      int ni = block_size*(nodei - 1);
+      int nj = block_size*(nodej - 1);
+
+      Ka.block<block_size,block_size>(ni,ni) = Ke[i].block<block_size,block_size>(0,0);
+      Ka.block<block_size,block_size>(ni,nj) = Ke[i].block<block_size,block_size>(0,block_size);
+      Ka.block<block_size,block_size>(nj,ni) = Ke[i].block<block_size,block_size>(block_size,0);
+      Ka.block<block_size,block_size>(nj,nj) = Ke[i].block<block_size,block_size>(block_size,block_size);
     }
+    return Ka;
+  }
 
+  auto solve(Eigen::MatrixXd Ka,const int& nnodes){
+
+    double beta = 10e9*Ka.maxCoeff();
+    //std::cout << Ka << '\n';
+    
+    Eigen::MatrixXd Ka_sol = Ka;
+    Ka_sol(15,15) += beta;
+    Ka_sol(16,16) += beta;
+
+    Ka_sol(18,18) += beta;
+    Ka_sol(19,19) += beta;
+
+    F = Equations::force_vector(md,nnodes);
+
+    U = Ka_sol.inverse() * F;
+
+    return std::make_pair(F,U);
+  }
+
+  auto postprocessing(Eigen::MatrixXd U,const std::vector<Element> elements){
+
+    epsilon.resize(elements.size());
+    
+    for(int i = 0; i < elements.size(); ++i){
+      
+      int ni = elements[i].node0;
+      int nj = elements[i].node1;
+
+      // Nodal displacements
+      double ui = U(Equations::number_of_unknowns*ni - 3);
+      double vi = U(Equations::number_of_unknowns*ni - 2);
+      double uj = U(Equations::number_of_unknowns*nj - 3);
+      double vj = U(Equations::number_of_unknowns*nj - 2);
+      
+      double ui_prime = l[i]*ui + m[i]*vi;
+      double uj_prime = l[i]*uj + m[i]*vj;
+
+      // Elongation and strain
+      double delta = uj_prime - ui_prime;
+      epsilon(i) = delta/L[i];
+    } // end loop through elements
+
+    // Axial stress
+    Eigen::VectorXd axial = E*epsilon;
+
+    std::cout << "Axial stress\n\n" << axial << '\n';
+    return axial.maxCoeff();
   }
 
 private:
@@ -296,45 +381,27 @@ private:
   std::vector<double>          m;
   std::vector<Eigen::MatrixXd> R;
   std::vector<Eigen::MatrixXd> Ke;
+  Eigen::MatrixXd              Ka;
+  Eigen::VectorXd              U;
+  Eigen::VectorXd              F;
+  Eigen::VectorXd              epsilon;
   double POD = 25.4;
-  double PID = 19.304;
+  double PID = 19.0;
   double SOD = 25.4;
   double SID = 20.638;
   double md  = 110;
-  
-  /*
-  struct Node{
-    int id;
-    double x;
-    double y;
-    double z;
-  };
-  
-  struct Element{
-    int id;
-    int node0;
-    int node1;
-    int matflag;
-  };
-  */
 
 };
-
-
 
 int main()
 {
 
 #ifdef TESTING
   double POD = 25.4;
-  double PID = 19.304;
+  double PID = 19.0;
   double SOD = 25.4;
   double SID = 20.638;
   double md  = 110;
-
-  auto newsol = Solver<TwoDBeam>(POD,PID,SOD,SID,md);
-#else
-  auto newsol = Solver<TwoDBeam>();
 #endif
  
 #ifdef TESTING
@@ -345,8 +412,8 @@ int main()
     std::tie(nodes,elements) = read_csv(nodal_filename,elemental_filename);
 #endif
 
-    newsol.preprocessing(nodes,elements);
-    auto operators = operations<double>();
-
+    auto newsol = Solver<TwoDBeam>(POD,PID,SOD,SID,md);
+    auto SF = newsol.SolveSystem(nodes,elements);
+    
   return 0;
 }
